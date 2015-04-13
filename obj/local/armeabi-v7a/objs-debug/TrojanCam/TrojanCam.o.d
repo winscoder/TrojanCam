5768f36c421ea07cc60a79089129666234ddb5c5
winscoder <zedyuang@gmail.com>
2015-04-13 00:20:39 -0700

    Add more files

---
+++ obj/local/armeabi-v7a/objs-debug/TrojanCam/TrojanCam.o.d
@@ -0,0 +1,157 @@
+obj/local/armeabi-v7a/objs-debug/TrojanCam/TrojanCam.o: jni/TrojanCam.cpp \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/jni.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/cdefs.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/android/api-level.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/vector \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/stdio.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/types.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/stdint.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/_types.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/_types.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/posix_types.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/stddef.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/compiler.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm/posix_types.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm/types.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/types.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/kernel.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h \
+ /opt/NVKit/NVPACK/OpenCV-2.4.8.2-Tegra-sdk/sdk/native/jni/include/opencv2/core/core.hpp \
+ /opt/NVKit/NVPACK/OpenCV-2.4.8.2-Tegra-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/assert.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/stdlib.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/string.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/malloc.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/alloca.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/strings.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/memory.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/math.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/limits.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/limits.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/limits.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/internal_types.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/limits.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/syslimits.h \
+ /opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm/page.h \
+ /opt/NVKit/NVPACK/OpenCV-2.4.8.2-Tegra-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+ /opt/NVKit/NVPACK/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm \
+ /opt/NVKit/NVPACK/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cmath \
+ /opt/NVKit/NVPACK/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cstddef \
+ /opt/NVKit/NVPACK/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/complex \
+ /opt/NVKit/NVPACK/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/map \
+ /opt/NVKit/NVPACK/OpenCV-2.4.8.2-Tegra-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+ /opt/NVKit/NVPACK/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/limits \
+ /opt/NVKit/NVPACK/OpenCV-2.4.8.2-Tegra-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+ /opt/NVKit/NVPACK/OpenCV-2.4.8.2-Tegra-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc.hpp \
+ /opt/NVKit/NVPACK/OpenCV-2.4.8.2-Tegra-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h \
+ /opt/NVKit/NVPACK/OpenCV-2.4.8.2-Tegra-sdk/sdk/native/jni/include/opencv2/core/core_c.h \
+ /opt/NVKit/NVPACK/OpenCV-2.4.8.2-Tegra-sdk/sdk/native/jni/include/opencv2/features2d/features2d.hpp \
+ /opt/NVKit/NVPACK/OpenCV-2.4.8.2-Tegra-sdk/sdk/native/jni/include/opencv2/flann/miniflann.hpp \
+ /opt/NVKit/NVPACK/OpenCV-2.4.8.2-Tegra-sdk/sdk/native/jni/include/opencv2/flann/defines.h \
+ /opt/NVKit/NVPACK/OpenCV-2.4.8.2-Tegra-sdk/sdk/native/jni/include/opencv2/flann/config.h \
+ jni/org_trojancam_TrojanCam.h
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/jni.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/cdefs.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/android/api-level.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/vector:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/stdio.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/types.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/stdint.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/_types.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/_types.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/posix_types.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/stddef.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/compiler.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm/posix_types.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm/types.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/types.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/kernel.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h:
+
+/opt/NVKit/NVPACK/OpenCV-2.4.8.2-Tegra-sdk/sdk/native/jni/include/opencv2/core/core.hpp:
+
+/opt/NVKit/NVPACK/OpenCV-2.4.8.2-Tegra-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/assert.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/stdlib.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/string.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/malloc.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/alloca.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/strings.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/memory.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/math.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/limits.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/limits.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/limits.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/internal_types.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/limits.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/syslimits.h:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm/page.h:
+
+/opt/NVKit/NVPACK/OpenCV-2.4.8.2-Tegra-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cmath:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cstddef:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/complex:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/map:
+
+/opt/NVKit/NVPACK/OpenCV-2.4.8.2-Tegra-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/opt/NVKit/NVPACK/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/limits:
+
+/opt/NVKit/NVPACK/OpenCV-2.4.8.2-Tegra-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+
+/opt/NVKit/NVPACK/OpenCV-2.4.8.2-Tegra-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc.hpp:
+
+/opt/NVKit/NVPACK/OpenCV-2.4.8.2-Tegra-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h:
+
+/opt/NVKit/NVPACK/OpenCV-2.4.8.2-Tegra-sdk/sdk/native/jni/include/opencv2/core/core_c.h:
+
+/opt/NVKit/NVPACK/OpenCV-2.4.8.2-Tegra-sdk/sdk/native/jni/include/opencv2/features2d/features2d.hpp:
+
+/opt/NVKit/NVPACK/OpenCV-2.4.8.2-Tegra-sdk/sdk/native/jni/include/opencv2/flann/miniflann.hpp:
+
+/opt/NVKit/NVPACK/OpenCV-2.4.8.2-Tegra-sdk/sdk/native/jni/include/opencv2/flann/defines.h:
+
+/opt/NVKit/NVPACK/OpenCV-2.4.8.2-Tegra-sdk/sdk/native/jni/include/opencv2/flann/config.h:
+
+jni/org_trojancam_TrojanCam.h:
